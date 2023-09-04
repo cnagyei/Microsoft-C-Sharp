d8a6dbb4ee46e0e3024ce3fb3f92cfecde1f60f8
cnagyei <cnagyei15@gmail.com>
2023-09-04 23:50:29 +0000

    updates

--- CSharp/CsharpProjects/TestProject/Program.cs
+++ CSharp/CsharpProjects/TestProject/Program.cs
@@ -1,23 +1,7 @@
-﻿using System.Globalization;
+﻿int firstValue = 500;
+int secondValue = 600;
+int largerValue;
 
-Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine("=====");
-
-int number = 7;
-string text = "seven";
-
-Console.WriteLine(number);
-Console.WriteLine();
-Console.WriteLine(text);
-
-Random toss = new Random();
-int roll1 = toss.Next();
-int roll2 = toss.Next(101);
-int roll3 = toss.Next(50, 101);
-
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+Console.WriteLine(largerValue);
