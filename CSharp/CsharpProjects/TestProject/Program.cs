34be63b3bd57f01b8a4c5830774d7dfd78efb5e1
cnagyei <cnagyei15@gmail.com>
2023-09-09 23:05:22 +0000

    dice game 2

--- CSharp/CsharpProjects/TestProject/Program.cs
+++ CSharp/CsharpProjects/TestProject/Program.cs
@@ -10,22 +10,30 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll1 == roll3) || (roll2 == roll3))
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total");
-    total += 2;
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total");
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total");
+        total += 2;
+    }
 }
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-    Console.WriteLine("You rolled triples! +6 bonus to total");
-    
+    Console.WriteLine("You win a new car!");
 }
-
-if (total >= 15)
+else if (total >= 10)
 {
-    Console.WriteLine("You win!");    
+    Console.WriteLine("You win a new laptop!");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-    Console.WriteLine("Sorry, you lose.");
+    Console.WriteLine("You win a trip!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
 }
